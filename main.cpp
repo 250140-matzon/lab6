7cec7018602489fb6ac4d5fb10759f1331877157
CS102_W3L1 <250140@newuu.uz>
2025-10-02 12:56:32 +0500

    created

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+// void myfunction() {
+//     int a,b;
+//     cin>>a>>b;
+//     cout<<a*b;
+// }
+//
+// int main() {
+//     myfunction();
+//     return 0;
+// }
+
+// void acc() {
+//     int a,b,t;
+//     cin>>a>>b>>t;
+//     cout<<(b-a)/t<<endl;
+// }
+//
+// int main() {
+//     acc();
+//     return 0;
+// }
+
+
+
+// void fun() {
+//     int r;
+//     float pi=3.141592653589;
+//     cin>>r;
+//     cout<<"Circumferance"<<2*pi*r<<endl;
+//     cout<<'Area'<<pi*r*r<<endl;
+// }
+//
+// int main() {
+//     fun();
+//     return 0;
+// }
+
